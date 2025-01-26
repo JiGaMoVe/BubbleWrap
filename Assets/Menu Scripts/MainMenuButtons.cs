8fc5e7a18c4565fc6822537d1cb961d195ce2853
SebasXLR <megasxlrsebas2@hotmail.com>
2025-01-26 13:01:33 -0300

    Main Menu

---
+++ Assets/Menu Scripts/MainMenuButtons.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    [SerializeField] private GameObject credits;
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void CreditsButton()
+    {
+        credits.SetActive(true);
+    }
+}
